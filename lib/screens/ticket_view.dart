120872393f23c57c960a8509dd8fc24400a62814
Emaga <ultrangamo@yahoo.fr>
2022-07-15 23:51:38 +0100

    still on ticket_view.dart

--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -1,5 +1,6 @@
 import 'package:booktickets/utils/app_layout.dart';
 import 'package:booktickets/utils/app_styles.dart';
+import 'package:booktickets/widgets/thick_container.dart';
 import 'package:flutter/material.dart';
 
 class TicketView extends StatelessWidget {
@@ -19,16 +20,65 @@ class TicketView extends StatelessWidget {
             Container(
               decoration: const BoxDecoration(
                 color: Color(0xFF526799),
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(21), topRight: Radius.circular(21)),
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(21),
+                    topRight: Radius.circular(21)),
               ),
               padding: const EdgeInsets.all(16),
               child: Column(
                 children: [
                   Row(
                     children: [
-                      Text("NYC", style: Styles.headLineStyle3.copyWith(color: Colors.white)),
-                      const Spacer(),
-                      Text("London", style: Styles.headLineStyle3.copyWith(color: Colors.white)),
+                      Text("NYC",
+                          style: Styles.headLineStyle3
+                              .copyWith(color: Colors.white)),
+                      Expanded(child: Container()),
+                      const ThickContainer(),
+                      Expanded(
+                        child: Stack(
+                          children: [
+                            SizedBox(
+                              height: 24,
+                              child: LayoutBuilder(
+                                builder: (BuildContext context,
+                                    BoxConstraints constraints) {
+                                  return Flex(
+                                    direction: Axis.horizontal,
+                                    mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                    mainAxisSize: MainAxisSize.max,
+                                    children: List.generate(
+                                      (constraints.constrainWidth() / 6).floor(),
+                                          (index) => const SizedBox(
+                                        width: 3,
+                                        height: 1,
+                                        child: DecoratedBox(
+                                          decoration:
+                                          BoxDecoration(color: Colors.white),
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
+                            Center(
+                              child: Transform.rotate(
+                                angle: 1.5,
+                                child: const Icon(
+                                  Icons.local_airport_rounded,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const ThickContainer(),
+                      Expanded(child: Container()),
+                      Text("London",
+                          style: Styles.headLineStyle3
+                              .copyWith(color: Colors.white)),
                     ],
                   ),
                 ],
